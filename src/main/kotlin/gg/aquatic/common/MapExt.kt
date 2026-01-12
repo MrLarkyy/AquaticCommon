89461b544cd8876a582a649e002e38c7e61faddb
MrLarkyy_ <studiotadikl@gmail.com>
2026-01-12 02:36:38 +0100

    Add Kotlin utility extensions for maps, numbers, and BigDecimal formatting
    - Introduced a `map` extension for transforming map entries into a new mutable map.
    - Added number formatting extensions for `Double` and `Float` to adjust decimal places.
    - Implemented `toBlockCardinal` extension for `Float` to determine cardinal directions.
    - Created multiple extensions for `BigDecimal` to simplify balance formatting, including compact, raw, and suffixed representations.
    - Added a utility to parse suffixed balances back into `BigDecimal`.

---
+++ src/main/kotlin/gg/aquatic/common/MapExt.kt
@@ -0,0 +1,12 @@
+package gg.aquatic.common
+
+import kotlin.collections.iterator
+
+inline fun <A,B,C,D> Map<A,B>.map(transform: (A,B) -> Pair<C,D>): MutableMap<C,D> {
+    val result = mutableMapOf<C,D>()
+    for (entry in this) {
+        val (c,d) = transform(entry.key, entry.value)
+        result[c] = d
+    }
+    return result
+}
